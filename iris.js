1a078b45ec9a70aec055f7c0d6947962932444af
Molnár Attila <molnar.attila@szlgbp.hu>
2025-11-13 12:45:12 +0100

    first commit

---
+++ iris.js
@@ -0,0 +1,29 @@
+function koordinatarendszer_keszitese(){
+    for (let i = 0; i <= 1000; i+=100) {
+        vonal([i,0],[i,25], "eros");
+        vonal([0,i],[25,i], "eros");        
+    }
+
+    for (let i = 0; i <= 1000; i+=100) {
+        vonal([i,0],[i,1000], "halvany");
+        vonal([0,i],[1000,i], "halvany");
+    }
+
+}
+
+
+
+
+// ez egy olyan függvény, ami a vászonra rajzol két pontpár közé egy vonalat.
+function vonal(P,Q, klassz=""){
+    let v = document.createElementNS('http://www.w3.org/2000/svg','line');;     //<line />
+    v.setAttribute('x1', P[0]); //<line x1="0"/>
+    v.setAttribute('y1', P[1]); //<line x1="0", y1="0" />
+    v.setAttribute('x2', Q[0]); //<line x1="0", y1="0" x2="1000" />
+    v.setAttribute('y2', Q[1]); //<line x1="0", y1="0" x2="1000" y2="1000" />
+    v.classList.add(klassz);
+    vaszon.appendChild(v);
+}
+
+
+koordinatarendszer_keszitese();
