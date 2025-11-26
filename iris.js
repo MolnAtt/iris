c37606f22381119bbd1bc70bd4aec4a6544f04cf
Molnár Attila <molnar.attila@szlgbp.hu>
2025-11-26 10:43:39 +0100

    katt

--- iris.js
+++ iris.js
@@ -62,10 +62,13 @@ function beolvas(){
     return noszirmok;
 }
 
+const mertekx = 300;
+const merteky = 120;
+
 function plot_petal(e){
     let noszirmok = beolvas();
     for (const noszirom of noszirmok) {
-        let P = [noszirom.petal_width*300, noszirom.petal_length*120];
+        let P = [noszirom.petal_width*mertekx, noszirom.petal_length*merteky];
         let r = 5;
         if(noszirom.tipus == 'Iris-setosa'){
             kor(P, r, 'borzas');
@@ -80,7 +83,56 @@ function plot_petal(e){
     }
 }
 
+
+
+function tavolsag(P, Q){
+    return Math.sqrt((P[0]-Q[0])*(P[0]-Q[0]) + (P[1]-Q[1])*(P[1]-Q[1]));
+}
+
+function sorbarendezes(lista, X, Y ){
+    let rendezett_lista = lista.slice();
+    rendezett_lista.sort( (I1,I2) => {
+        let egyik_tavolsaga = tavolsag([I1.petal_length, I1.petal_width], [X,Y]);
+        let masik_tavolsaga = tavolsag([I2.petal_length, I2.petal_width], [X,Y]);
+        if(egyik_tavolsaga < masik_tavolsaga)
+            return -1;
+        else if (egyik_tavolsaga == masik_tavolsaga)
+            return 0;
+        return 1
+    });
+    return rendezett_lista;
+}
+
+function k_nearest_neighbours(this_petal_length, this_petal_width, K){
+    // supervised learning
+
+    // 1. vesszük a K db legközelebbi pontot.
+    // 1.1      sorbarendezed a távolságképlet alapján a pontokat
+    // 1.2      Megnézed az első K db elemet.
+    // 2. Megnézzük, hogy melyik milyen típus.
+    // 2.1      Csoportosítás/dictionary...
+    // 3. A legtöbb "szavazatot" kapó típusra tippelünk.
+    // 3.1      Maximumkeresés a dictionary-n
+
+}
+
+
 petal_gomb.addEventListener('click', plot_petal); // html-ben nevezd el petal_gomb-nak az id-t!
 
 koordinatarendszer_keszitese();
+
+
+function svg_katt(e){
+    e.preventDefault();
+    let br = vaszon.getBoundingClientRect();
+    let a = {"X": e.clientX - br.x, "Y": e.clientY - br.y};
+    console.log(a);
+    let petal_width  = a.X/mertekx;
+    let petal_length = a.Y/merteky;
+    let nsz_t = k_nearest_neighbours(petal_width, petal_length, 5);
+    
+    console.log(nsz_t);
+}
+
+vaszon.addEventListener('click', svg_katt);
 
